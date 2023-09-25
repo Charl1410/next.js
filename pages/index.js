d6ef507310998fcf82967e27f92327f96163250b
Charlotte Griffin <charlottegriffin@Charlottes-MacBook-Pro.local>
2023-09-25 12:39:18 +0100

    added homepage image

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
-import Link from "next/link";
+import Image from "next/image";
+
 
 
 export default function Home() {
@@ -12,7 +13,12 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <p>This is the homepage</p>
-
+      <Image
+        src="/images/banner.jpg"
+        alt="My Image"
+        width={1300} // Set the desired width
+        height={400} // Set the desired height
+      />
       <section className={utilStyles.headingMd}></section>
     </Layout>
   );
