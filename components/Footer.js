119187a72c86563fe5b52e175a6a5c42bed395b0
Charlotte Griffin <charlottegriffin@Charlottes-MacBook-Pro.local>
2023-10-01 14:16:09 +0100

    got data pulling through into the product listgit add -A

--- components/Footer.js
+++ components/Footer.js
@@ -5,26 +5,29 @@ const Footer = () => {
   return (
     <footer className={styles.container}>
       {/* about section one of 3*/}
-            <section className={styles.section}>
-            <h5>Contact Us</h5>
-            <ul className={styles.ul}>
-                <li>urna</li>
-                <li>ornare</li>
-                <li>ornare</li>
-                <li>ornare</li>
-                <li>ornare</li>
-            </ul>
-            </section>
-            <section className={styles.section}>
-            <h5>Returns</h5>
-            <ul className={styles.ul}>
-                <li>urna</li>
-                <li>ornare</li>
-                <li>ornare</li>
-                <li>ornare</li>
-                <li>ornare</li>
-            </ul>
-            </section>
+
+      <div className={styles.links}>
+        <section className={styles.section}>
+          <h5>Contact Us</h5>
+          <ul className={styles.ul}>
+            <li>urna</li>
+            <li>ornare</li>
+            <li>ornare</li>
+            <li>ornare</li>
+            <li>ornare</li>
+          </ul>
+        </section>
+        <section className={styles.section}>
+          <h5>Returns</h5>
+          <ul className={styles.ul}>
+            <li>urna</li>
+            <li>ornare</li>
+            <li>ornare</li>
+            <li>ornare</li>
+            <li>ornare</li>
+          </ul>
+        </section>
+      </div>
     </footer>
   );
 }
