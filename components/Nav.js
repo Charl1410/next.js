cc762f3d40e1edd1d512b30fd2e286c203bbf02c
Charlotte Griffin <charlottegriffin@Charlottes-MacBook-Pro.local>
2023-10-11 16:11:46 +0100

    restyled navbar

--- components/Nav.js
+++ components/Nav.js
@@ -4,28 +4,37 @@ import styles from '../styles/nav.module.css'
   // components/Navbar.js
 const Navbar = () => {
   return (
-    <nav className={styles.navbar}>
-      <ul className={styles.navList}>
-        <li className={styles.navItem}>
-          <Link href="/">
-            <i className="material-icons">home</i>
-          </Link>
-        </li>
-        <li className={styles.navItem}>
-          <Link href="/blog">Blog</Link>
-        </li>
-        <li className={styles.navItem}>
-          <Link href="/products">Products</Link>
-        </li>
-        <li className={styles.navItem}>
-          <Link href="/cart">Cart</Link>
-        </li>
-      </ul>
+    <div className={styles.navContainer}>
+      <nav className={styles.navbar}>
+        <ul className={styles.navList}>
+          <li className={styles.navItem}>
+            <Link href="/">
+              <i className="material-icons">home</i>
+            </Link>
+          </li>
+          <li className={styles.navItem}>
+            <Link href="/blog">Blog</Link>
+          </li>
+          <li className={styles.navItem}>
+            <Link href="/products">Products</Link>
+          </li>
+          <li className={styles.navItem}>
+            <Link href="/cart">Cart</Link>
+          </li>
+        </ul>
+      </nav>
       <div className={styles.searchContainer}>
-        <input className={styles.input} type="text" placeholder="Search..."></input>
+        <input
+          className={styles.input}
+          type="text"
+          placeholder="Search..."
+        ></input>
         <i className="material-icons">search</i>
       </div>
-    </nav>
+      <Link href="/cart">
+        <i className="material-icons">shopping_cart</i>
+      </Link>
+    </div>
   );
 };
 
